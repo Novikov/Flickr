61bc5c6ebba73894f5bf25470d577cea55c6a663
Igor Novikov <igor.novikov@umbrellait.com>
2022-10-05 13:49:29 +0300

    self review

--- data/src/main/java/com/app/data/remote_data_source/api_service/interestingness/di/InterestingnessModule.kt
+++ data/src/main/java/com/app/data/remote_data_source/api_service/interestingness/di/InterestingnessModule.kt
@@ -1,7 +1,6 @@
 package com.app.data.remote_data_source.api_service.interestingness.di
 
 import com.app.data.remote_data_source.api_service.interestingness.InterestingnessApiService
-import com.app.domain.models.interestingness.response.Photos
 import dagger.Module
 import dagger.Provides
 import retrofit2.Retrofit
@@ -11,10 +10,5 @@ object InterestingnessModule {
     @Provides
     fun provideInterestingnessApiService(retrofit: Retrofit): InterestingnessApiService {
         return retrofit.create(InterestingnessApiService::class.java)
-    }
-
-    @Provides
-    fun providesPhotos(): Photos? {
-        return null
     }
 }

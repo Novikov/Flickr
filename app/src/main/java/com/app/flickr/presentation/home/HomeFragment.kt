3e5f603f83aece8cb3174cebbbd7b2da394f2e17
Igor Novikov <igor.novikov@umbrellait.com>
2022-10-06 13:11:32 +0300

    navigation to photo search

--- app/src/main/java/com/app/flickr/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/app/flickr/presentation/home/HomeFragment.kt
@@ -5,8 +5,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.app.flickr.R
 import com.app.flickr.databinding.FragmentHomeBinding
@@ -54,12 +56,30 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initRecycler()
+        initListeners()
         setObservers()
     }
 
     private fun initRecycler() {
         viewBinding?.photoRecycler?.layoutManager = GridLayoutManager(context, GRID_IMAGES_COUNT)
         viewBinding?.photoRecycler?.adapter = photosAdapter
+    }
+
+    private fun initListeners() {
+        viewBinding?.toolbar?.root?.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.item_search -> {
+                    // TODO: Igor think how to implement effect
+                    // TODO: Igor think how to add basic navigation cases
+                    findNavController().navigate(R.id.searchFragment)
+                    true
+                }
+                else -> {
+                    Toast.makeText(requireContext(), "saved photos", Toast.LENGTH_SHORT).show()
+                    false
+                }
+            }
+        }
     }
 
     private fun setObservers() {
---
+++ app/src/main/java/com/app/flickr/presentation/search/SearchFragment.kt
@@ -0,0 +1,6 @@
+package com.app.flickr.presentation.search
+
+import androidx.fragment.app.Fragment
+import com.app.flickr.R
+
+class SearchFragment : Fragment(R.layout.fragment_search)
